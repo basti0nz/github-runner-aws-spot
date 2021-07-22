be6c56b30da4d7a35ad49396a930031771adde53
Valentyn Nastenko <valentyn.nastenko@restream.io>
2021-07-22 16:37:36 +0300

    fix error

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,28 +1,27 @@
-
 export interface IEC2Params {
-	instanceId?: string
-	ec2ImageId?: string
-	ec2InstanceType?: string
-	subnetId?: string
-	securityGroupId?: string
-	iamRoleName?: string
-	tags?: string
-	githubOwner?: string
-	githubRepo?: string
-	label?: string
-	githubRegistrationToken?: string
+  instanceId?: string
+  ec2ImageId?: string
+  ec2InstanceType?: string
+  subnetId?: string
+  securityGroupId?: string
+  iamRoleName?: string
+  tags?: string
+  githubOwner?: string
+  githubRepo?: string
+  label?: string
+  githubRegistrationToken?: string
 }
 
 export interface AWSWorker {
-	startEc2Instance(): Promise<string>
-	waitForInstanceRunning(instanceId: string): void
-	terminateEc2Instance(): void
+  startEc2Instance(): Promise<string>
+  waitForInstanceRunning(): void
+  terminateEc2Instance(): void
 }
 
 export interface GitHubWorker {
-	getRegistrationToken(): Promise<any>
-	getRunner(): Promise<null | any>
-	removeRunner(): void
-	waitForRunnerRegistered(label: string): void
-
+  /* eslint-disable  @typescript-eslint/no-explicit-any */
+  getRegistrationToken(): Promise<any>
+  getRunner(): Promise<null | any>
+  removeRunner(): void
+  waitForRunnerRegistered(): void
 }
--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,54 @@
 import * as core from '@actions/core'
-import { awsClient } from './aws'
-import { gitHubClient } from './github'
+import {awsClient} from './aws'
+import {gitHubClient} from './github'
 import * as github from '@actions/github'
-import { IEC2Params } from './interfaces'
+import {IEC2Params} from './interfaces'
 
 function genLabel(): string {
-  return Math.random().toString(36).substr(2, 5);
+  return Math.random().toString(36).substr(2, 5)
 }
 
 async function startRunner(token: string, params: IEC2Params): Promise<string> {
   const ghc = new gitHubClient(token, params.label!, github.context)
-  const githubRegistrationToken = await ghc.getRegistrationToken();
+  params.githubRegistrationToken = await ghc.getRegistrationToken()
   const aws = new awsClient(params)
-  const ec2InstanceId = await aws.startEc2Instance();
-  await aws.waitForInstanceRunning(ec2InstanceId);
-  await ghc.waitForRunnerRegistered(params.label!);
+  const ec2InstanceId = await aws.startEc2Instance()
+  await aws.waitForInstanceRunning()
+  await ghc.waitForRunnerRegistered()
   return ec2InstanceId
 }
 
-async function stopRunner(token: string, label: string, ec2InstanceId: string) {
-  let params: IEC2Params = {
-    instanceId: ec2InstanceId,
+async function stopRunner(
+  token: string,
+  label: string,
+  ec2InstanceId: string
+): Promise<void> {
+  const params: IEC2Params = {
+    instanceId: ec2InstanceId
   }
   const aws = new awsClient(params)
-  await aws.terminateEc2Instance();
+  await aws.terminateEc2Instance()
 
   const ghc = new gitHubClient(token, label, github.context)
-  await ghc.removeRunner();
-
+  await ghc.removeRunner()
 }
 
 async function run(): Promise<void> {
   try {
-
     const mode = core.getInput('mode')
     if (!mode) {
-      throw new Error(`The 'mode' input is not specified`);
+      throw new Error(`The 'mode' input is not specified`)
     }
 
     const ghToken: string = core.getInput('github-token')
     if (!ghToken) {
-      throw new Error(`The 'github-token' input is not specified`);
+      throw new Error(`The 'github-token' input is not specified`)
     }
 
     //core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
     if (mode === 'start') {
-      let params: IEC2Params = {
+      const params: IEC2Params = {
         ec2ImageId: core.getInput('ec2-image-id'),
         ec2InstanceType: core.getInput('ec2-instance-type'),
         subnetId: core.getInput('subnet-id'),
@@ -56,23 +58,32 @@ async function run(): Promise<void> {
         label: genLabel()
       }
 
-      if (!params.ec2ImageId || !params.ec2InstanceType || !params.subnetId || !params.securityGroupId) {
-        throw new Error(`Not all the required inputs are provided for the 'start' mode`)
+      if (
+        !params.ec2ImageId ||
+        !params.ec2InstanceType ||
+        !params.subnetId ||
+        !params.securityGroupId
+      ) {
+        throw new Error(
+          `Not all the required inputs are provided for the 'start' mode`
+        )
       }
 
       const ec2InstanceId = await startRunner(ghToken, params)
-      core.setOutput('label', params.label);
-      core.setOutput('ec2-instance-id', ec2InstanceId);
+      core.setOutput('label', params.label)
+      core.setOutput('ec2-instance-id', ec2InstanceId)
       return
     } else if (mode === 'stop') {
       const label = core.getInput('label')
       const ec2InstanceId = core.getInput('ec2-instance-id')
       if (!label || !ec2InstanceId) {
-        throw new Error(`Not all the required inputs are provided for the 'stop' mode`)
+        throw new Error(
+          `Not all the required inputs are provided for the 'stop' mode`
+        )
       }
       await stopRunner(ghToken, label, ec2InstanceId)
     } else {
-      throw new Error('Wrong mode. Allowed values: start, stop.');
+      throw new Error('Wrong mode. Allowed values: start, stop.')
     }
   } catch (error) {
     core.setFailed(error.message)
